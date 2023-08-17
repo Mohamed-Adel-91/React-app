1ec80030c67b5a25c80b543a8909ece880830898
MohamedAdel <mohamed101291@gmail.com>
2023-08-17 20:05:05 +0300

    if rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,17 @@
 function ListGroup() {
-    const items = ["NewYork", "London", "Paris", "Tokyo"];
+    let items = ["NewYork", "London", "Paris", "Tokyo"];
+    items = [];
+    // const Message = items.length === 0 ? <p>No Items found</p> : null;
+    // const getMessage = () => {
+    //     return items.length === 0 ? <p>No Items found</p> : null;
+    // };
     return (
         <>
             <h1>List</h1>
+            {/* {items.length === 0 ? <p>No Items found</p> : null} */}
+            {items.length === 0 && <p>No Items Found</p>}
+            {/* {Message} */}
+            {/* {getMessage()} */}
             <ul className="list-group">
                 {items.map((item) => (
                     <li key={item}>{item}</li>
