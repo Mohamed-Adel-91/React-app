b3fb858fa6dcc07bb16123480478f11ed8affdbc
MohamedAdel <mohamed101291@gmail.com>
2023-09-08 08:23:54 +0300

    css-js & eslint fix

--- src/components/ListGroup2/ListGroup2.tsx
+++ src/components/ListGroup2/ListGroup2.tsx
@@ -2,35 +2,41 @@ import { useState } from "react";
 import "./ListGroup2.css";
 import styled from "styled-components";
 
+interface ListItemsProps {
+    active: boolean;
+}
+
 const List = styled.ul`
     list-style: none;
     padding: 0;
     margin-top: 15px;
 `;
 
-const ListItems = styled.li`
-    font-size: 50px;
+const ListItems = styled.li<ListItemsProps>`
+    font-size: 20px;
     padding: 5px 0;
+    background: ${(props) => (props.active ? "blue" : "none")};
 `;
-
 interface ListGroupProps {
     items: string[];
     heading: string;
     onSelectItems: (item: string) => void;
 }
-function ListGroup2(props: ListGroupProps) {
-    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+function ListGroup2({ items, heading, onSelectItems }: ListGroupProps) {
+    const [selectedIndex, setSelectedIndex] = useState(0);
     return (
         <>
-            <h1>{props.heading}</h1>
-            {props.items.length === 0 && <p>No Items Found</p>}
+            <h1>{heading}</h1>
+            {items.length === 0 && <p>No Items Found</p>}
             <List>
-                {props.items.map((item, index) => (
+                {items.map((item, index) => (
                     <ListItems
+                        active={index === selectedIndex}
                         key={item}
                         onClick={() => {
                             setSelectedIndex(index);
-                            props.onSelectItems(item);
+                            onSelectItems(item);
                         }}
                     >
                         {item}
