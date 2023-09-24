a3768033000396cc92630973a7d15a5e2bd3b400
MohamedAdel <mohamed101291@gmail.com>
2023-09-24 23:12:33 +0300

    useState Ex

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import produce from "immer";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
+import ExpandableText from "./components/ExpandableText";
 
 let items = ["NewYork", "London", "Paris", "Tokyo"];
 const handleSelectedItems = (item: string) => {
@@ -19,17 +20,26 @@ function App() {
     const [alertVisible, setAlertVisible] = useState(false);
     // const [isApproved, setApproved] = useState(true);
 
+    // Handle event for useState
+    let count = 0;
+    const handleClick = () => {
+        setAlertVisible(true);
+        count++;
+        console.log(alertVisible);
+        console.log(count);
+    };
+    //************************************************************************** */
     // string useState
     // const [firstName, setFirstName] = useState('');
     // const [lastName, setLastName] = useState('');
     // const fullName = firstName + ' ' + lastName;
-
+    //************************************************************************** */
     // object useState
     // const [person, setPerson]  = useState({
     //     firstName : '',
     //     lastName : '',
     // })
-
+    //************************************************************************** */
     // Nested object useState
     const [drink, setDrink] = useState({
         title: "Americano",
@@ -39,32 +49,15 @@ function App() {
         },
         price: 5,
     });
-
-    // array useState
-    const [tags, setTags] = useState(["Happy", "Cheerful"]);
-
-    // array of object useState
-    const [bugs, setBugs] = useState([
-        { id: 1, title: "Bug 1", fixed: false },
-        { id: 2, title: "Bug 2", fixed: false },
-    ]);
-
-    // Handle event for useState
-    let count = 0;
-    const handleClick = () => {
-        setAlertVisible(true);
-        count++;
-        console.log(alertVisible);
-        console.log(count);
-    };
-
     const handlePrice = () => {
         setDrink({ ...drink, price: 6 });
     };
-
     const handleQnt = () => {
         setDrink({ ...drink, adds: { ...drink.adds, Qnt: 170 } });
     };
+    //************************************************************************** */
+    // array useState
+    const [tags, setTags] = useState(["Happy", "Cheerful"]);
 
     const handleTags = () => {
         // Add to Array
@@ -78,13 +71,18 @@ function App() {
 
         console.log(tags);
     };
-
+    //************************************************************************** */
+    // array of object useState
+    const [bugs, setBugs] = useState([
+        { id: 1, title: "Bug 1", fixed: false },
+        { id: 2, title: "Bug 2", fixed: false },
+    ]);
     const handleBugs = () => {
         // setBugs(
         //     bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug))
         // );
         // console.log(bugs);
-
+        //********************************** */
         // handle with immer
         setBugs(
             produce((draft) => {
@@ -93,10 +91,10 @@ function App() {
             })
         );
     };
-
+    //************************************************************************** */
     // Handle sharing state between component
     const [cartItems, setCartItems] = useState(["Product 1", "Product 2"]);
-
+    //************************************************************************** */
     // Ex1 on useState
     const [game, setGame] = useState({
         id: 1,
@@ -108,57 +106,109 @@ function App() {
         setGame({ ...game, player: { ...game.player, names: "pop" } });
         console.log(game);
     };
+    //************************************************************************** */
+    // Ex2 on useState
+    const [pizza, setPizza] = useState({
+        name: "spicy Pepperoni",
+        topping: ["Mushroom"],
+    });
+    const handlePizza = () => {
+        setPizza({ ...pizza, topping: [...pizza.topping, "Cheese"] });
+        console.log(pizza);
+    };
+    //************************************************************************** */
+    // Ex3 on useState
+    const [cart, setCart] = useState({
+        discount: 0.1,
+        items: [
+            { id: 1, title: "P1", quantity: 1 },
+            { id: 2, title: "P2", quantity: 1 },
+        ],
+    });
+    const handleCart = () => {
+        setCart({
+            ...cart,
+            items: cart.items.map((item) =>
+                item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+            ),
+        });
+        console.log(cart);
+    };
+
     return (
-        <div>
-            <button onClick={handleGame}>game</button>
-            <br />
-            <NavBar cartItemsCount={cartItems.length} />
-            <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
-            <br />
-            {bugs.map((bug) => (
-                <p key={bug.id}>
-                    {bug.title} {bug.fixed ? "Fixed" : "New"}
-                </p>
-            ))}
-            <button onClick={handleBugs}>fix bugs</button>
-            <br />
-            {tags.length}
-            <button onClick={handleTags}>tags</button>
-            <br />
-            {drink.adds.Qnt}
-            <button onClick={handleQnt}>Qnt</button>
-            <br />
-            {drink.price}
-            <button onClick={handlePrice}>price</button>
-            <br />
-            <button onClick={handleClick}>Show</button>
-            <br />
-            {alertVisible && (
-                <Alert onClose={() => setAlertVisible(false)}>
-                    This is an <span>alert</span> box!
-                </Alert>
-            )}
-            <br />
-            {/* {fullName} */}
-            <Button color="primary" onClick={() => setAlertVisible(true)}>
-                Button test
-            </Button>
-            <br />
-            <Like onClick={() => console.log("Like Clicked")} />
-            <br />
-            <ListGroup
-                items={items}
-                heading="Cities"
-                onSelectItems={handleSelectedItems}
-            />
-            <br />
-            <ListGroup2
-                items={items}
-                heading={"Cities Group 2"}
-                onSelectItems={handleSelectedItems}
-            ></ListGroup2>
-            <br />
-        </div>
+        <>
+            <div>
+                <button onClick={handleCart}>cart</button>
+                <br />
+                <button onClick={handlePizza}>pizza</button>
+                <br />
+                <button onClick={handleGame}>game</button>
+                <br />
+                <NavBar cartItemsCount={cartItems.length} />
+                <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+                <br />
+                {bugs.map((bug) => (
+                    <p key={bug.id}>
+                        {bug.title} {bug.fixed ? "Fixed" : "New"}
+                    </p>
+                ))}
+                <button onClick={handleBugs}>fix bugs</button>
+                <br />
+                {tags.length}
+                <button onClick={handleTags}>tags</button>
+                <br />
+                {drink.adds.Qnt}
+                <button onClick={handleQnt}>Qnt</button>
+                <br />
+                {drink.price}
+                <button onClick={handlePrice}>price</button>
+                <br />
+                <button onClick={handleClick}>Show</button>
+                <br />
+                {alertVisible && (
+                    <Alert onClose={() => setAlertVisible(false)}>
+                        This is an <span>alert</span> box!
+                    </Alert>
+                )}
+                <br />
+                {/* {fullName} */}
+                <Button color="primary" onClick={() => setAlertVisible(true)}>
+                    Button test
+                </Button>
+                <br />
+                <Like onClick={() => console.log("Like Clicked")} />
+                <br />
+                <ListGroup
+                    items={items}
+                    heading="Cities"
+                    onSelectItems={handleSelectedItems}
+                />
+                <br />
+                <ListGroup2
+                    items={items}
+                    heading={"Cities Group 2"}
+                    onSelectItems={handleSelectedItems}
+                ></ListGroup2>
+                <br />
+            </div>
+            <div>
+                <ExpandableText>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Voluptate esse perferendis enim corrupti! Architecto velit
+                    sed dolore. Odit a eum expedita nisi, culpa earum quae
+                    laboriosam quos, molestiae beatae maiores doloremque soluta
+                    delectus placeat architecto praesentium pariatur atque. Et,
+                    itaque repudiandae. Accusamus laudantium iusto ut, cum iste
+                    earum aspernatur quas facere quo, non nostrum ea asperiores
+                    natus ratione. Quibusdam quod minima, voluptatibus laborum
+                    corporis error repellendus explicabo commodi dolor et,
+                    impedit repudiandae, asperiores exercitationem illum quas ad
+                    adipisci expedita consequatur nihil? Autem minus, quisquam
+                    nihil quia dolorum fugit temporibus error officiis qui nisi
+                    aspernatur doloremque, esse quos libero sed nulla.
+                </ExpandableText>
+            </div>
+        </>
     );
 }
 
---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+interface Props {
+    children: string;
+    maxChars?: number;
+}
+
+const ExpandableText = ({ children, maxChars = 100 }: Props) => {
+    const [isExtend, setExtend] = useState(false);
+    const handelExtend = () => {
+        setExtend(!isExtend);
+    };
+
+    if (children.length <= maxChars) return <p>{children}</p>;
+
+    const text = isExtend ? children : children.substring(0, maxChars);
+    return (
+        <>
+            <p>
+                {text}...
+                <button onClick={handelExtend}>
+                    {isExtend ? "Less" : "More"}
+                </button>
+            </p>
+        </>
+    );
+};
+
+export default ExpandableText;
