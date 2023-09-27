10a5e87453df4f41bf54cb5deeaf238076bcbe55
MohamedAdel <mohamed101291@gmail.com>
2023-09-27 23:42:44 +0300

    handle forms

--- src/components/ReactHookForm.tsx
+++ src/components/ReactHookForm.tsx
@@ -1,7 +1,17 @@
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+    name: string;
+    age: number;
+}
+
 const ReactHookForm = () => {
-    const { register, handleSubmit } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormData>();
+
     const onSubmit = (data: FieldValues) => console.log(data);
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -11,11 +21,19 @@ const ReactHookForm = () => {
                     Name
                 </label>
                 <input
-                    {...register("name")}
+                    {...register("name", { required: true, minLength: 3 })}
                     id="name"
                     type="text"
                     className="form-control"
                 />
+                {errors.name?.type === "required" && (
+                    <p className="text-danger">The Name Field is required.</p>
+                )}
+                {errors.name?.type === "minLength" && (
+                    <p className="text-danger">
+                        The Name Must be at least 3 Characters.
+                    </p>
+                )}
             </div>
             {/* div.mb-3>label.form-label+input[type=number].form-control */}
             <div className="mb-3">
